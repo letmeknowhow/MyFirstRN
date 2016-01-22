b7120324c20d31f6e7a36b874b3ab40720050f59
nxy <nxy801111@hotmail.com>
2016-01-22 17:23:36 +0800

    code push

--- bundleAndroid.sh
+++ bundleAndroid.sh
@@ -6,6 +6,7 @@ react-native bundle \
 --platform android \
 --entry-file index.android.js \
 --bundle-output ./release/android/index.android.bundle \
+--assets-dest ./release/android \
 --dev false
 # 因为codepush尚不支持 文件夹发布,所以不能以文件夹的方式发布
 code-push release wealth-android  ./release/android/index.android.bundle  1.0.0
--- bundleios.sh
+++ bundleios.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 rm -rf ./release/ios/*
-
+mkdir -p ./release/ios
 react-native bundle \
 --platform ios \
 --entry-file index.ios.js \
 --bundle-output ./release/ios/main.jsbundle \
+--assets-dest ./release/ios \
 --dev false
 # ios下codepush支持文件夹发布
-code-push release wealth-ios  ./release/ios  1.0.0
+code-push release NxyApp-ios  ./release/ios  1.0.0
